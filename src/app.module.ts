de5f31920436eb8e641a40d1c35a85c33639d67e
Gaston Muñoz <gaston.m@flydevs.com>
2022-07-24 16:49:29 -0300

    Added mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common'
 import { join } from 'path'
 import { ServeStaticModule } from '@nestjs/serve-static'
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
+      serveRoot: '/public',
       rootPath: join(__dirname, '..', 'public'),
     }),
+    PokemonModule,
   ],
   controllers: [],
   providers: [],
